70eaf5d7b53e06e74fe614834171c311cc83df10
TianLuLu <lulu.tian@kfwgd.com>
2017-09-05 18:28:32 +0800

    Initial Commit

---
+++ JPUSHDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JPUSHDemo
+//
+//  Created by TianLuLu on 2017/9/5.
+//  Copyright © 2017年 TianLuLu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
